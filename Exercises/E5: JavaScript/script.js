20313482f4d267ba8435f15dda1f7554160c9049
lilyruble <lilyruble01@gmail.com>
2023-10-22 23:31:11 -0400

    java

--- Exercises/E5: JavaScript/script.js
+++ Exercises/E5: JavaScript/script.js
@@ -1,34 +1,66 @@
+
+//Solution for Loop//
 let name = "loop";
-let letter = "p"
+
+
+let loopcontainer = document.querySelector('#loopContainer')
+for (let step = 0; step < 10; step++) {
+    let textBox = document.createElement("p");
+    textBox.innerHTML = name
+    loopcontainer.appendChild(textBox);
+}
+
+
+// Solution for Interaction Color//
+
+const colorBtns = document.querySelectorAll(".colors");
+
+for (const colorBtn of colorBtns) {
+    colorBtn.addEventListener("click", function () {
+        interactionContainer.style.backgroundColor = getComputedStyle(colorBtn).backgroundColor;
+    })
+}
+
+//Solution for Condition//
+
+let position = document.getElementById("conditionContainer");
+let square = document.getElementById("square");
+
+position.addEventListener('mousemove', color);
+
+function color(e) {
+    if (e.offsetY > 300) {
+        square.style.backgroundColor = "lightred";
+    } else {
+        square.style.backgroundColor = "lightpurple";
+    }
+}
+
+//Solution for Increase
+
+const content = document.getElementById("increaseText");
+const text = 20;
+function Increase() {
+    if (text < 120) {
+        text++;
+        content.style.fontSize = text + "px";
+    }
+}
+
+setInterval(Increase, 1000);
+
+
+//Solution for Input
+const textLength = document.getElementById("text-length");
+const submitBtn = document.getElementById("submit");
+const inputText = document.getElementById("inputText");
+submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    const charCount = inputText.value.length;
+    textLength.textContent = charCount;
+})
 
 
 
-// for (let step = 0; step < 50; step++) {
-//     let loop = document.createElement("p");
-//     document.getElementById("loopContainer").innerHTML = "loop" + letter;
-// }
-
-//Solution 1//
-
-// const interactionContainer = document.getElementById("interactionContainer")
-
-// document.getElementById("green").addEventListener("click", function () {
-//     console.log("Clicked on on green circle")
-//     interactionContainer.style.backgroundColor = "lightgreen"
-// })
-
-// document.getElementById("plum").addEventListener("click", function () {
-//     console.log("Clicked on on plum circle")
-//     interactionContainer.style.backgroundColor = "plum"
-// })
-
-// document.getElementById("blue").addEventListener("click", function () {
-//     console.log("Clicked on on light blue circle")
-//     interactionContainer.style.backgroundColor = "lightblue"
-// })
-
-
-
-
-
+//Solution for Console//
 console.log("Wooohoooo I finished my assignment!")
