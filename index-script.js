9c1e5bb0c4e323cc0388813c4058c6c563a8468a
lilyruble <lilyruble01@gmail.com>
2023-12-01 14:49:20 -0500

    mouse and rotations

--- index-script.js
+++ index-script.js
@@ -1,15 +1,48 @@
-let images = [
-    'imgs/Readings.png',
-    'imgs/links.png',
-    'imgs/PROJECTS/interaction.png',
+// Mouse Animation
+// https://www.youtube.com/watch?v=XdHXOzNubxc&t=188s&ab_channel=TheWebnuts-OnlineTutorials
+Splitting();
 
+let cursor = document.querySelector('.cursor'),
+    cursorText = cursor.querySelectorAll('.char');
 
+// Split characters and animate in a circle
+function rounded(radius) {
 
-];
+    for (let i = 0; i < cursorText.length; i++) {
+        let rotation = i * (360 / cursorText.length);
+        gsap.set(cursorText[i], {
+            transformOrigin: `0px ${radius}px`,
+            x: radius,
+            rotate: rotation
+        });
+        gsap.set(cursor, { transformOrigin: "center center", rotation: 0, width: radius * 2, height: radius * 2 })
 
-// document.getElementsByClassName('elements');
-const container = document.getElementsByClassName('container')
-images.forEach(image => {
-    const img = document.createElement('img');
-    container.appendChild(img);
+    }
+
+    let rotate = gsap.timeline({ repeat: -1 })
+    rotate.to(cursor, { rotation: 360, duration: 5, ease: "none", })
+}
+
+let radius = 70
+
+// Mouse Movement function - How and where it can move
+function cursorMove(e) {
+    var mouseX = e.pageX
+    mouseY = e.pageY
+    tl = gsap.timeline();
+    tl.to(cursor, {
+        duration: 0.5,
+        x: mouseX - radius,
+        y: mouseY - radius,
+        ease: Expo.ease
+    })
+}
+
+function init() {
+    rounded(radius);
+    window.addEventListener('mousemove', cursorMove);
+}
+
+window.addEventListener("load", function () {
+    init();
 })
